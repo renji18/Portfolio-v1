a6e5c28cf93fc7764b3eb2ab488cf0307bb3c3ba
renji18 <aadarshjha1401@gmail.com>
2023-04-18 02:08:08 +0530

    user counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import Watashi from "../assets/IMG-20230215-WA0007.jpg";
+import axios from "axios";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const Header = ({ activeMenu, setActiveMenu }) => {
   const [userTheme, setUserTheme] = useState();
+  const [userCount, serUserCount] = useState(0);
 
   const ulElements = [
     { title: "Home", route: "/" },
@@ -15,6 +19,22 @@ const Header = ({ activeMenu, setActiveMenu }) => {
   ];
 
   useEffect(() => {
+    const userCounter = async () => {
+      const { data } = await axios.get(
+        "https://portfolio-2cf75-default-rtdb.firebaseio.com/userCount.json"
+      );
+
+      if (sessionStorage.getItem("aadarsh-portfolio-counter") === null) {
+        sessionStorage.setItem("aadarsh-portfolio-counter", true);
+        const res = await axios.put(
+          "https://portfolio-2cf75-default-rtdb.firebaseio.com/userCount.json",
+          { counter: data.counter + 1 }
+        );
+        serUserCount(res.data.counter);
+      } else {
+        serUserCount(data.counter);
+      }
+    };
     const themeCheck = () => {
       if (
         localStorage.theme === "dark" ||
@@ -28,8 +48,9 @@ const Header = ({ activeMenu, setActiveMenu }) => {
         document.documentElement.classList.remove("dark");
       }
     };
+    userCounter();
     themeCheck();
-  }, [userTheme]);
+  }, [userTheme, userCount]);
 
   const themeSwitch = () => {
     if (document.documentElement.classList.contains("dark")) {
@@ -43,6 +64,21 @@ const Header = ({ activeMenu, setActiveMenu }) => {
     localStorage.setItem("theme", "dark");
   };
 
+  const ordinalSuffix = (i) => {
+    var j = i % 10,
+      k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  };
+
   return (
     <div className="sm:flex text-xl border-b border-b-[#323232] dark:border-b-[#178582] sticky font-serif hidden w-[100vw] items-center justify-around bg-[#DDD0C8] dark:bg-[#0A1828] text-[#323232] dark:text-[#BFA181] py-5">
       <p className="text-2xl cursor-pointer">
@@ -51,9 +87,6 @@ const Header = ({ activeMenu, setActiveMenu }) => {
         </Link>
       </p>
       <ul className="flex gap-7">
-        <div className="cursor-pointer" style={{transform: 'translateY(-1px)'}} onClick={themeSwitch}>
-          {localStorage.theme === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
-        </div>
         {ulElements &&
           ulElements.map((item) => (
             <li
@@ -74,6 +107,28 @@ const Header = ({ activeMenu, setActiveMenu }) => {
             </li>
           ))}
       </ul>
+      <div className="flex justify-center gap-5 items-center">
+        <div
+          className="cursor-pointer"
+          style={{ transform: "translateY(-1px)" }}
+          onClick={themeSwitch}
+        >
+          {localStorage.theme === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
+        </div>
+        <div
+          data-tooltip-id="user-count"
+          id="user-count"
+          className="bg-[#DDD0C8] rounded-full px-2 pb-2 dark:bg-[#0A1828] text-[#323232] dark:text-[#BFA181] border-[#323232] dark:border-[#178582] cursor-pointer border"
+        >
+          {userCount}
+          <Tooltip
+            anchorId="user-count"
+            content={`Your are the ${ordinalSuffix(
+              userCount
+            )} viewer of my portfolio. Thank you!!!`}
+          />
+        </div>
+      </div>
     </div>
   );
 };
