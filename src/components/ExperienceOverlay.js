d0a0fb43c14f8d3f67288925550dc066f7337d17
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-08-23 15:53:07 +0530

    experience section firse

--- src/components/ExperienceOverlay.js
+++ src/components/ExperienceOverlay.js
@@ -11,16 +11,10 @@ const ExperienceOverlay = ({ data, handleCloseOverlay }) => {
   return (
     <>
       <div
-        className="fixed top-0 right-0 left-0 bottom-0 z-40 bg-black/60"
+        className="fixed top-0 right-0 left-0 bottom-0 z-40 bg-black/80"
         onClick={handleCloseOverlay}
       ></div>
-      <div className="fixed top-20 right-20 left-20 bottom-20 z-50 bg-white">
-        <div
-          className="absolute top-[-15px] text-3xl italic right-[-15px] bg-black rounded-full px-2 cursor-pointer"
-          onClick={handleCloseOverlay}
-        >
-          X
-        </div>
+      <div className="fixed flex flex-col lg:flex-row top-20 p-5 right-20 left-20 bottom-20 z-50 bg-black">
       </div>
     </>
   )
